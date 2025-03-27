b53914bf8d1087e31f39c516bafa9b2ad2b1d961
swordshik <beforlifeandmoon@gmail.com>
2025-03-27 01:47:39 -0400

    deez

--- art of teaching comp. science/mode.py
+++ art of teaching comp. science/mode.py
@@ -1,6 +1,7 @@
+
 def finding_mode(lst):
     mode = max(set(lst), key = lst.count)
     return mode
 
-numbers = [float(i) for i in input().split()]
+numbers = [float(i) for i in input('Write a list of numbers:\n').split()]
 print(f'Mode of the given list = {finding_mode(numbers)}')
